b38f34978cc0007c983d034d4123d921526810d1
Excel619 <excel8392@gmail.com>
2023-12-04 12:59:11 -0500

    Updated build.gradle.kts to improved superproject version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,3 @@
-val artifactName = "doors"
-val rrGroup: String by rootProject.extra
-val rrVersion: String by rootProject.extra
-
-plugins {
-    `java-library`
-    `maven-publish`
-}
-
-group = rrGroup
-version = rrVersion
-
 dependencies {
     compileOnly(commonLibs.paper)
     compileOnly(commonLibs.acf)
@@ -18,19 +6,4 @@ dependencies {
     compileOnly(commonLibs.worldguardevents)
     compileOnly(project(":Projects:Core"))
     compileOnly(project(":Projects:Common"))
-}
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = rrGroup
-            artifactId = artifactName
-            version = rrVersion
-            from(components["java"])
-        }
-    }
 }
