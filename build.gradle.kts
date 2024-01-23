ef30ab580ea09ba79acebe6e19638f78a4d99d32
Excel619 <excel8392@gmail.com>
2024-01-23 16:57:42 -0500

    Updated worldguardlegacy -> worldguardbukkit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     compileOnly(commonLibs.paper)
     compileOnly(commonLibs.acf)
     compileOnly(commonLibs.worldguardcore)
-    compileOnly(commonLibs.worldguardlegacy)
+    compileOnly(commonLibs.worldguardbukkit)
     compileOnly(commonLibs.worldguardevents)
     compileOnly(project(":Projects:Core"))
     compileOnly(project(":Projects:Common"))
