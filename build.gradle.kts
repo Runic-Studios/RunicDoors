1457e3f18e99cd73fc0413aa6b75a2a8a320af7d
Excel619 <excel8392@gmail.com>
2024-01-23 16:41:59 -0500

    RR584 Initial UNFINISHED UNCOMPILABLE progress

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
     compileOnly(commonLibs.paper)
     compileOnly(commonLibs.acf)
     compileOnly(commonLibs.worldguardcore)
-    compileOnly(commonLibs.worldguardlegacy)
     compileOnly(commonLibs.worldguardevents)
     compileOnly(project(":Projects:Core"))
     compileOnly(project(":Projects:Common"))
